9ce5e1332b6f4eed512e92685ca5c670a6d1873f
CsCheems <j.Dev.mx@outlook.com>
2025-08-29 17:51:33 -0600

    se cambio server

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const socket = io('https://imageuploader-back-buqn.onrender.com');
+//const socket = io('https://imageuploader-back-buqn.onrender.com');
+const socket = io('https://image-uploader-back.vercel.app/');
+
 
 socket.on('imagen-actualizada', (dataUrl) => {
     document.getElementById('miImagen').src = dataUrl;
