631ea1fe2148c9c2a0b1e3c15a84e554b395b0ba
CsCheems <j.Dev.mx@outlook.com>
2025-08-29 18:59:32 -0600

    se restauro

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//const socket = io('https://imageuploader-back-buqn.onrender.com');
-const socket = io('https://image-uploader-back.vercel.app');
+const socket = io('https://imageuploader-back-buqn.onrender.com');
+//const socket = io('https://image-uploader-back.vercel.app');
 
 
 socket.on('imagen-actualizada', (dataUrl) => {
